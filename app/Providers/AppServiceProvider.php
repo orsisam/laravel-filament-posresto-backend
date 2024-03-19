6967dc0b7d2c581a7b1e9300166ca447e2161c64
Isro Hidayatulloh <orsi.amax@gmail.com>
2024-03-20 00:51:09 +0700

    CRUD Category

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // config()->set('app.url', 'http://192.168.1.13:8001');
     }
 }
